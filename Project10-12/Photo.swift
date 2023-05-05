79e32435388c5528a512b4cc44033bb8e741fabf
fauzandp <fauzan27dp@gmail.com>
2023-05-05 14:56:51 +0700

    Project10-12 | Photo Gallery App design UI in storyboad add Photo class implement tableView method

---
+++ Project10-12/Photo.swift
@@ -0,0 +1,20 @@
+//
+//  Photo.swift
+//  Project10-12
+//
+//  Created by Fauzan Dwi Prasetyo on 05/05/23.
+//
+
+import Foundation
+
+class Photo {
+    
+    var imageName: String
+    var caption: String
+    
+    init(imageName: String, caption: String) {
+        self.imageName = imageName
+        self.caption = caption
+    }
+    
+}
--- Project10-12/ViewController.swift
+++ Project10-12/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var photos = [Photo]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Photo Gallery"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .organize, target: self, action: #selector(addPhoto))
+        
     }
-
-
+    
+    @objc func addPhoto() {
+        
+    }
+    
 }
 
+
+extension ViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let photo = photos[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath)
+        
+        cell.textLabel?.text = photo.caption
+        
+        return cell
+    }
+    
+}
